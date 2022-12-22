56b39e20a4e7eaacbaca45bcb03b2cf8a8602beb
Priyakarn20 <priyakarn1997@gmail.com>
2022-12-22 09:02:13 +0530

    adding ifel

---
+++ practice-work/ifel.js
@@ -0,0 +1,41 @@
+var a = 20;
+if(a>10){
+    console.log("a is greater than 10");
+};
+
+var b = 50;
+if (b==10){
+console.log("b is equal to 10")
+}
+else {
+    console.log("b is not equal to 10")
+}
+var c = 20;
+if(c/2==10){
+    console.log("statement verified")
+}
+else { "statement is wrong "}
+var d = 100;
+if (d/5==10){
+    console.log("d/5=10")
+}
+else if(d/10==10){
+    console.log("d/10=10")
+}
+else if(d/5==20){
+    console.log("d/5=20")
+}
+else{ console.log('print nothing'
+)}
+var e = 50;
+if(e==10){
+    console.log("a = 10")
+}
+else if (e==15){
+    console.log("e = 15")
+}
+else if(e==20){
+    console.log("e = 20")
+}
+else{ console.log('print nothing'
+)}
